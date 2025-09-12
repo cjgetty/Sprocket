e231167194db015c3297b2721d562666b5995db6
Cameron Getty <cameronjgetty@gmail.com>
2025-09-11 23:37:05 -0400

    Initial Commit

---
+++ Sprocket/SprocketApp.swift
@@ -0,0 +1,20 @@
+//
+//  SprocketApp.swift
+//  Sprocket
+//
+//  Created by Cameron Getty on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SprocketApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
